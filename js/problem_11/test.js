f9da4087a9deafb2d068c17a2cd3e56e388557c5
melisayu <melisayuliakristin@gmail.com>
2019-06-25 03:38:27 +0700

    Fix long lines

--- js/problem_11/test.js
+++ js/problem_11/test.js
@@ -2,9 +2,17 @@ var assert = require('assert');
 var solution = require('../problem_11/solution')
 
 describe('Problem #11', function() {
-    it('Test prefix', function() {
-        assert.deepEqual(solution.findMatchingString(['dog', 'deer', 'deal'], 'de'), ['deer', 'deal'])
-        assert.deepEqual(solution.findMatchingString(['sword', 'sigil', 'knight', 'scythe', 'dragon'], 's'),
-            ['sword', 'sigil', 'scythe'])
+    it('Test prefix 1', function() {
+        var q = 'de'
+        var a = ['dog', 'deer', 'deal']
+        var expected = ['deer', 'deal']
+        assert.deepEqual(solution.findMatchingString(a, q), expected)
+    })
+
+    it('Test prefix 2', function() {
+        var q = 's'
+        var a = ['sword', 'sigil', 'knight', 'scythe', 'dragon']
+        var expected = ['sword', 'sigil', 'scythe']
+        assert.deepEqual(solution.findMatchingString(a, q), expected)
     })
 });

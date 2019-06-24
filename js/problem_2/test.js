00cf84f409358b05e80d86f68d724d556868c4ea
melisayu <melisayuliakristin@gmail.com>
2019-06-25 02:59:46 +0700

    fix the test

--- js/problem_2/test.js
+++ js/problem_2/test.js
@@ -2,9 +2,9 @@ var assert = require('assert');
 var solution = require('../problem_2/solution')
 
 describe('Problem #2', function() {
-    it('Test data with division', function() {
-        assert.equal(solution.products([1, 2, 3, 4, 5], [120, 60, 40, 30, 24]))
-        assert.equal(solution.products([3, 2, 1], [2, 3, 6]))
+    it('Test data without division', function() {
+        assert.deepEqual(solution.products([1, 2, 3, 4, 5]), [120, 60, 40, 30, 24])
+        assert.deepEqual(solution.products([3, 2, 1]), [2, 3, 6])
     })
 });
 

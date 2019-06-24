ba2cd89ea7da8984560de3444cb32e424b4a28d3
melisayu <melisayuliakristin@gmail.com>
2019-06-25 03:39:46 +0700

    Fix long lines

--- js/problem_11/test.js
+++ js/problem_11/test.js
@@ -5,14 +5,14 @@ describe('Problem #11', function() {
     it('Test prefix 1', function() {
         var q = 'de'
         var a = ['dog', 'deer', 'deal']
-        var expected = ['deer', 'deal']
-        assert.deepEqual(solution.findMatchingString(a, q), expected)
+        var result = ['deer', 'deal']
+        assert.deepEqual(solution.findMatchingString(a, q), result)
     })
 
     it('Test prefix 2', function() {
         var q = 's'
         var a = ['sword', 'sigil', 'knight', 'scythe', 'dragon']
-        var expected = ['sword', 'sigil', 'scythe']
-        assert.deepEqual(solution.findMatchingString(a, q), expected)
+        var result = ['sword', 'sigil', 'scythe']
+        assert.deepEqual(solution.findMatchingString(a, q), result)
     })
 });
